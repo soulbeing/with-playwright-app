75f6c0008ccd62260e0f31a72b0d8ece7c040749
Sidney <xjiang@shutterstock.com>
2023-10-11 11:01:26 -0400

    add github action

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test'
 
 test('should navigate to the about page', async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
+  // await page.goto("http://localhost:3000/")
   await page.goto('/')
   // Find an element with the text 'About Page' and click on it
   await page.getByText('About Page').click()
